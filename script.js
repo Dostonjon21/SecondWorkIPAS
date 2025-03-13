6ca2242e99e494319605f5e3d82996ecea2fb7e9
Doston12 <rajabovdost@gmail.com>
2025-03-13 21:06:46 +0500

    ad

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
- const getPhotoData = () => {
-            fetch('https://jsonplaceholder.typicode.com/photos/2')
-                .then(response => response.json())
-                .then(data => {
-                    const container = document.getElementById('photo-box');
+// 'use strict'
+// const Img = () => {
+//     const request = new XMLHttpRequest();
+//     request.open('GET', 'https://jsonplaceholder.typicode.com/photos');
+//     request.send();
+
+//     request.addEventListener('load', function () {
+//         const data = JSON.parse(this.responseText);
+//         console.log(' :', data);
+
+       
+//     });
+// };
+
+
+// Img();
+
+
+
+// const getPhotoData = () => {
+//     fetch('https://jsonplaceholder.typicode.com/photos/2')
+//         .then(response => response.json())
+//         .then(data => {
+//             const container = document.getElementById('photo-box');
+//             container.innerHTML = `
+//                 <img src="${data.url}" alt="${data.title}">
+//                 <h3>${data.title}</h3>
+//             `;
+//         })
+//         .catch(error => {
+//             console.error('Error fetching data:', error);
+//         });
+// };
+
+// getPhotoData();
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const getPhotoData = () => {
+        fetch('https://jsonplaceholder.typicode.com/photos/2')
+            .then(response => response.json())
+            .then(data => {
+                const container = document.getElementById('photo-box');
+                if (container) {
                     container.innerHTML = `
-                        <img src="${data.url}" alt="${data.title}">
+                        <img src="${data.url}" alt="${data.title}" style="max-width: 100%; height: auto;">
                         <h3>${data.title}</h3>
                     `;
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        };
+                } else {
+                    console.error("Element topilmadi!");
+                }
+            })
+            .catch(error => {
+                console.error('Xatolik yuz berdi:', error);
+            });
+    };
 
-        getPhotoData();
+    getPhotoData();
+});
