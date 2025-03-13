844e4b57491f8500f68bc75511d4182ef68965ef
Doston12 <rajabovdost@gmail.com>
2025-03-13 21:11:52 +0500

    add

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-'use strict'
-const Img = () => {
-    const request = new XMLHttpRequest();
-    request.open('GET', 'https://jsonplaceholder.typicode.com/photos');
-    request.send();
+// 'use strict'
+// const Img = () => {
+//     const request = new XMLHttpRequest();
+//     request.open('GET', 'https://jsonplaceholder.typicode.com/photos');
+//     request.send();
 
-    request.addEventListener('load', function () {
-        const data = JSON.parse(this.responseText);
-        console.log(' :', data);
+//     request.addEventListener('load', function () {
+//         const data = JSON.parse(this.responseText);
+//         console.log(' :', data);
 
        
-    });
-};
+//     });
+// };
 
 
 // Img();
